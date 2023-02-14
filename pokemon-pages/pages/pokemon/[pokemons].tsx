f72578672a8b5651dacc9961499b44003550a535
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-02-14 12:05:08 +0100

    Added pagination

--- pokemon-pages/pages/pokemon/[pokemons].tsx
+++ pokemon-pages/pages/pokemon/[pokemons].tsx
@@ -1,3 +1,5 @@
+import { useRouter } from 'next/router';
+
 type PokemonApiName = {
 	data: {
 		name: string;
@@ -5,7 +7,42 @@ type PokemonApiName = {
 };
 
 function Pokemon(props: PokemonApiName) {
-	return <h1>{props.data.name}</h1>;
+	const router = useRouter();
+	const fisrtPage = 1;
+	const lastPage = 19;
+	const pageNumber = Number(router.query.pokemons);
+	const paginationHandler = (symbol: string) => {
+		if (symbol === '+') {
+			router.push(`/pokemon/${pageNumber + 1}`);
+		}
+		if (symbol === '-') {
+			router.push(`/pokemon/${pageNumber - 1}`);
+		}
+	};
+	return (
+		<div
+			style={{
+				height: '100vh',
+				display: 'flex',
+				flexDirection: 'column',
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}>
+			<h1>{props.data.name}</h1>
+			<div style={{ display: 'flex', gap: 20 }}>
+				<button
+					onClick={() => paginationHandler('-')}
+					disabled={fisrtPage >= pageNumber}>
+					Prev
+				</button>
+				<button
+					onClick={() => paginationHandler('+')}
+					disabled={lastPage <= pageNumber}>
+					Next
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default Pokemon;
@@ -14,8 +51,8 @@ export async function getStaticPaths() {
 	const request = await fetch('https://pokeapi.co/api/v2/pokemon');
 	const data = (await request.json()) as { results: { name: string }[] };
 
-	const paths = data.results.map((pok) => ({
-		params: { pokemons: pok.name },
+	const paths = data.results.map((pok, index) => ({
+		params: { pokemons: index.toString() },
 	}));
 	return {
 		paths,
